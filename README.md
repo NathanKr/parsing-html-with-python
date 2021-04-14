d2421d6e5d0e0f5ffb727d924ed31862ddaae66f
Nathan Krasney Laptop <natankrasney@gmail.com>
2021-04-14 07:34:26 +0300

    better encoding handling

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# parsing-html-with-python
+<h2>Motivation</h2>
+Play with html in python
+
+<h2>Points of interest</h2>
+<ul>
+<li>parsing the html was used with lxml - no specific html code needed</li>
+<li>encoding in parse-simple-html.py was strange - i had to use unicode for the etree.tostring and utf-8 for open</li>
+</ul>
+
--- parse-simple-html.py
+++ parse-simple-html.py
@@ -1,15 +1,23 @@
 from lxml import etree
+import os
 from io import StringIO
 
-html_doc = "<html><head><title>test</head><body><h1>page title</h3></html>"
+# consume html string from lxml
+html_doc = "<html><head><title>test</head><body><h1>טקסט בעברית</h3></html>"
+to_string_encoding = 'unicode' #using utf-8 was not good here
 parser = etree.HTMLParser()
 htmlTree   = etree.parse(StringIO(html_doc), parser)
 print(f'type(htmlTree) : {type(htmlTree)}')
-strHtmlTree = etree.tostring(htmlTree.getroot(), encoding='unicode')
+strHtmlTree = etree.tostring(htmlTree.getroot(), encoding=to_string_encoding)
 print(f'type(strHtmlTree) : {type(strHtmlTree)}')
 print(strHtmlTree)
-file_path = 'doc.html'
-with open(file_path, 'w') as file: 
+
+# write html string to html file
+file_name = 'doc.html'
+file_path = os.path.join('.','result',file_name)
+
+file_write_encoding = 'utf-8' #using unicode was not good here
+with open(file_path, 'w' , encoding=file_write_encoding) as file: 
     print(file.write(strHtmlTree)) 
 
 
